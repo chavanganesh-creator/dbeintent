c707a5fcb2a4fbc27160c21119a274e712a1e894
Chavan <chavanganesh806@gmail.com>
2021-04-13 18:09:51 +0530

    init commit

---
+++ app/src/main/java/com/example/dbeintent/ui/report/ReportNextActivity.java
@@ -0,0 +1,53 @@
+package com.example.dbeintent.ui.report;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.example.dbeintent.R;
+
+public class ReportNextActivity extends AppCompatActivity {
+
+    private static final String TAG = "HomeNextActivity";
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_report_next);
+        Log.d(TAG, "onCreate: started.");
+
+        getIncomingIntent();
+    }
+
+    private void getIncomingIntent(){
+        Log.d(TAG, "getIncomingIntent: checking for incoming intents.");
+
+        if(getIntent().hasExtra("image_url") && getIntent().hasExtra("image_name")){
+            Log.d(TAG, "getIncomingIntent: found intent extras.");
+
+            String imageUrl = getIntent().getStringExtra("image_url");
+            String imageName = getIntent().getStringExtra("image_name");
+
+            setImage(imageUrl, imageName);
+        }
+    }
+
+
+    private void setImage(String imageUrl, String imageName){
+        Log.d(TAG, "setImage: setting te image and name to widgets.");
+
+        TextView name = findViewById(R.id.image_description);
+        name.setText(imageName);
+
+        ImageView image = findViewById(R.id.image);
+        Glide.with(this)
+                .asBitmap()
+                .load(imageUrl)
+                .into(image);
+    }
+}
